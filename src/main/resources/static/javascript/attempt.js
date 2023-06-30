3a13f653ee4ea941b8d9aa3f18361b83e5a02a84
Koy Dibb <dibbk5@gmail.com>
2023-06-30 11:25:11 -0600

    Scoring Quiz

--- src/main/resources/static/javascript/attempt.js
+++ src/main/resources/static/javascript/attempt.js
@@ -14,6 +14,7 @@ const nameValue = urlParams.get("quiz-name");
 // Find Elements
 const quizName = document.getElementById("quiz-name");
 const questionContainer = document.getElementById("question-container");
+const scoreContainer = document.getElementById("score-container");
 
 //Set array for correct answers
 let answersArray = [];
@@ -48,10 +49,12 @@ function createQuestionCard(array) {
     questionCard.classList.add("m-2");
     questionCard.innerHTML = `
           <div class="card d-flex" style="width: 75%; height: 10%; margin: 50px;">
-              <div class="card-header" id="question${obj.id}">Question ${
-      i + 1
-    }</div>
-                  <div class="card-body d-flex flex-column  justify-content-between" style="height: available">
+              <div class="card-header" id="question${
+                obj.id
+              }" name="question">Question ${i + 1}
+              </div>
+              <div class="card-body d-flex flex-column  justify-content-between" style="height: available">
+              <div class="card-title" id="question-mark${obj.id}"></div>
                       <p class="card-text">${obj.description}</p>
                           <div id="${obj.id}"></div> 
                   </div>
@@ -89,8 +92,9 @@ function createAnswerSection(array, questionId) {
 function submitQuiz() {
   let userScore = 0;
   let userAnswers = [];
-  let answerSelector = document.getElementsByName("answer");
-  console.log(answerSelector);
+  const answerSelector = document.getElementsByName("answer");
+  const questionSelector = document.getElementsByName("question");
+  const numberQuestions = questionSelector.length;
   for (i = 0; i < answerSelector.length; i++) {
     if (answerSelector[i].checked) {
       let idString = answerSelector[i].id;
@@ -98,17 +102,38 @@ function submitQuiz() {
       userAnswers.push(parseInt(answerId));
     }
   }
-  console.log(userAnswers);
   for (i = 0; i < answersArray.length; i++) {
     if (userAnswers[i] === answersArray[i].id) {
       userScore++;
+      let headerText = document.getElementById(
+        `question${answersArray[i].question}`
+      );
+      let markText = document.getElementById(
+        `question-mark${answersArray[i].question}`
+      );
+
+      headerText.classList.add("correct");
+      markText.innerText = "CORRECT";
+      markText.style.color = "green";
     } else {
       let headerText = document.getElementById(
         `question${answersArray[i].question}`
       );
+      let markText = document.getElementById(
+        `question-mark${answersArray[i].question}`
+      );
+
       headerText.classList.add("cross");
+      markText.innerText = "INCORRECT";
+      markText.style.color = "red";
     }
   }
+
+  const finalPercentage = (userScore / numberQuestions) * 100;
+
+  scoreContainer.innerHTML = `
+    <div class="container">SCORE: ${finalPercentage} %</div>
+    <div class="container">You got ${userScore} out of ${numberQuestions} quesitons correct.</div>`;
   console.log(userScore);
 }
 
